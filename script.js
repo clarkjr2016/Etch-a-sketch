639230ba12c4198f7b8bec9fe23b96f450800c29
Eugene Clark <clarkjr2016@gmail.com>
2022-10-29 14:05:30 -0400

    Grid manipulation using user input

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 const container = document.querySelector(".container");
 
-for (i = 0; i <= 15; i++) {
+
+
+
+const prompt = document.createElement("button");
+prompt.innerHTML = "Select Me";
+
+const finalPrompt = container.appendChild(prompt);
+
+const grid = container.appendChild(document.createElement("div"));
+
+grid.classList.add("grid");
+
+let gridSizeLength = getComputedStyle(grid).getPropertyValue("--length");
+let gridSizeWidth = getComputedStyle(grid).getPropertyValue("--width");
+
+
+
+finalPrompt.addEventListener("click", () => {
+    let input = window.prompt ("What's the grid size");
+    gridSizeLength = input;
+    gridSizeWidth = input;
+    
+    for (i=1; i <= input; i++) {
+        const gridItem = grid.appendChild(document.createElement("div"))
+        gridItem.classList.add("grid-item");
+        gridItem.innerHTML = input;
+    }   
+
+})
+
+
+
+/* create an onclick event that takes a prompt and uses that number to create the amount of grids 
+
+
+
+
+/* for (i = 0; i <= 15; i++) {
     let item = document.createElement("div");
     item.setAttribute("class", "child");
     container.append(item);
@@ -10,5 +47,5 @@ let grids = document.querySelectorAll(".child");
 
 grids.forEach(grid => grid.addEventListener("mouseover", (e) => {
     e.target.style.backgroundColor = "red";
-}));
+})); */
 
